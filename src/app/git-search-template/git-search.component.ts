9b77a5ba00feed6ec296b9c1f70912761d180531
restrada25 <restrad2@hawk.iit.edu>
2018-08-27 00:28:32 -0500

    Template driven form

---
+++ src/app/git-search-template/git-search.component.ts
@@ -0,0 +1,60 @@
+import { AdvancedSearchModel } from './../advanced-search-model';
+import { Component, OnInit } from '@angular/core';
+import { GitSearchService } from '../git-search.service';
+import { GitSearch } from '../git-search';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+@Component({
+  selector: 'app-git-search',
+  templateUrl: './git-search.component.html',
+  styleUrls: ['./git-search.component.css']
+})
+export class GitSearchComponent implements OnInit {
+  searchResults: GitSearch;
+  searchQuery: string;
+  displayQuery: string;
+  title: string;
+  constructor(private gitSearchService: GitSearchService, private route: ActivatedRoute, private router: Router ) { }
+
+  model = new AdvancedSearchModel('', '', '', null, null, '');
+  modelKeys = Object.keys(this.model);
+
+  ngOnInit() {
+    this.route.paramMap.subscribe( (params: ParamMap) => {
+      this.searchQuery = params.get('query');
+      this.displayQuery = params.get('query');
+      this.gitSearch();
+    });
+    this.route.data.subscribe( (result) => {
+      this.title = result.title;
+    });
+  }
+
+  gitSearch = () => {
+    this.gitSearchService.gitSearch(this.searchQuery).then( (response) => {
+      this.searchResults = response;
+    }, (error) => {
+      alert('Error: ' + error.statusText);
+    });
+  }
+
+  sendQuery = () => {
+    this.searchResults = null;
+    const search: string = this.model.q;
+    let params = '';
+    this.modelKeys.forEach( (elem) => {
+      if (elem === 'q') {
+        return false;
+      }
+      if (this.model[elem]) {
+        params += '+' + elem + ':' + this.model[elem];
+      }
+    });
+    this.searchQuery = search;
+    if (params !== '') {
+      this.searchQuery = search + '+' + params;
+    }
+    this.displayQuery = this.searchQuery;
+    this.gitSearch();
+  }
+
+}
